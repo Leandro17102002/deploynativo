9006e7c9da73ec32f43f4ce46b2eb896bc99a871
Leandro <leandrogpelozo1@gmail.com>
2026-02-17 21:40:38 -0300

    modificacion contact-form

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -69,7 +69,7 @@ const sendDataToServer = (data) => {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-  },
+    },
     body: JSON.stringify(data)
   })
   .then(response => response.json())
